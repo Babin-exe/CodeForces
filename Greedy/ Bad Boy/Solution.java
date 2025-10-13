def6b33a80be9353301a0bcd5ef28882ba79f386
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-10-13 16:19:03 +0545

    Create Solution.java

---
+++ Greedy/ Bad Boy/Solution.java
@@ -0,0 +1,20 @@
+// Problem Link : https://codeforces.com/problemset/problem/1537/B
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+
+            long n, m, i, j;
+            n = sc.nextLong();
+            m = sc.nextLong();
+            i = sc.nextLong();
+            j = sc.nextLong();
+            System.out.println("1 1 " + n + " " + m);
+        }
+        sc.close();
+    }
+}
