e1a9a9178fb139b6a0c6b96752f4b4fe7f1f9579
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-10-13 16:18:45 +0545

    Create Solution.cpp

---
+++ Greedy/ Bad Boy/Solution.cpp
@@ -0,0 +1,20 @@
+// Problem Link : https://codeforces.com/problemset/problem/1537/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    std::cin >> t;
+
+    while (t--)
+    {
+        long long n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << "1 1 " << n << " " << m << "\n";
+    }
+
+    return 0;
+}
